8efb66814c7c38189d1ab43fe8f31d5633effd69
Grokes <default1one@gmail.com>
2024-10-07 21:16:48 +0300

    Login

--- src/DOM/Login/LoginForm.js
+++ src/DOM/Login/LoginForm.js
@@ -1,26 +1,98 @@
 //import "./Login.css"
 
-function LoginForm() {
-    return[
-        <div className="LoginForm">
-            <form>
-                <h3 class="text-center">Форма входа</h3>
-                <div class="form-group">
-                    <input class="form-control item" type="text" name="username" maxlength="15" minlength="4" pattern="^[a-zA-Z0-9_.-]*$" id="username" placeholder="Логин" required/>
-                </div>
-                <div class="form-group">
-                    <input class="form-control item" type="password" name="Пароль" minlength="6" id="password" placeholder="Пароль" required/>
-                </div>
-                <div class="form-group">
-                    <input class="form-control item" type="email" name="email" id="email" placeholder="Email" required/>
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary btn-block create-account" type="submit">Вход в аккаунт</button>
-                </div>
+import {useState} from "react";
+import Checkers from "../Checkers";
+import { useNavigate } from 'react-router-dom';
 
+function LoginForm({ setIsAuthenticated }){
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        email: ''})
+    const navigate = useNavigate();
+    const users = [
+        {
+            username: "kirill",
+            password:"123456",
+            email:"123@gmail.com"},
+        {
+            username: "Dmitriy",
+            password:"123456",
+            email:"123@ya.ru"},
+        {
+            username: "Alex",
+            password:"123456",
+            email:"321@ya.ru"}
+    ]
+
+    function handleChange(event){
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log('Данные формы', formData)
+        console.log('Данные бд', users[0])
+
+        if (validate())
+        {
+            setIsAuthenticated(true)
+            navigate('/checkers')
+        }
+        else
+        {
+            console.log('fail')
+        }
+    }
+
+    function validate(){
+        for(let i = 0; i < users.length; i++)
+        if (formData.password === users[i].password
+            && formData.email === users[i].email
+            && formData.username === users[i].username)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    return(
+        <div className="LoginForm">
+            <form onSubmit={handleSubmit} onChange={handleChange}>
+                <h3 className="text-center">Форма входа</h3>
+                <div className="form-group">
+                    <input className="form-control item"
+                           type="text"
+                           name="username"
+                           maxLength="15"
+                           minLength="4"
+                           id="username"
+                           placeholder="Логин" required/>
+                </div>
+                <div className="form-group">
+                    <input className="form-control item"
+                           type="password"
+                           name="password"
+                           minLength="6"
+                           id="password"
+                           placeholder="Пароль" required/>
+                </div>
+                <div className="form-group">
+                    <input className="form-control item"
+                           type="email"
+                           name="email"
+                           id="email"
+                           placeholder="Email" required/>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary btn-block create-account"
+                            type="submit">Вход в аккаунт</button>
+                </div>
             </form>
         </div>
-    ];
+    );
 }
-
 export default LoginForm;
--- src/Loader.js
+++ src/Loader.js
@@ -1,28 +1,29 @@
-import LoginForm from "./DOM/Login/LoginForm";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import React from "react";
-import {useState} from "react"
+import {useState} from "react";
 import Checkers from "./DOM/Checkers";
+import LoginForm from "./DOM/Login/LoginForm";
 
 function Loader(){
-    const [page, setPage] = useState("login")
-    if (page === "login")
-    {
-        return  [<LoginForm />, <button onClick={NextIndex}>mhjgjgjhg</button>];
-    }
-    else if (page === "index")
-    {
-        return  [<Checkers />, <button onClick={NextLogin}>mhjgjgjhg</button>];
-    }
-    function NextLogin(event)
-    {
-        setPage("login")
-    }
-    function NextIndex(event)
-    {
-        setPage("index")
-    }
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/login"
+                    element={<LoginForm setIsAuthenticated={setIsAuthenticated} />}
+                />
+                <Route
+                    path="/checkers"
+                    element={isAuthenticated ? <Checkers /> : <Navigate to="/login" />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/login" />}
+                />
+            </Routes>
+        </Router>
+    );
 }
-
-
 
 export default Loader;
